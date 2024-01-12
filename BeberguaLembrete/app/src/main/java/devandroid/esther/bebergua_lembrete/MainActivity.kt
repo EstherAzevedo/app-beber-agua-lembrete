d81167d331cea412b8b9a33cdbd4edd5d8dca07a
EstherAzevedo/ <estherazvdo123@gmail.com>
2024-01-12 20:45:13 -0300

    Criação da lógica do cálculo de ingestão de água e do btn redefinir

--- BeberguaLembrete/app/src/main/java/devandroid/esther/bebergua_lembrete/MainActivity.kt
+++ BeberguaLembrete/app/src/main/java/devandroid/esther/bebergua_lembrete/MainActivity.kt
@@ -2,12 +2,73 @@ package devandroid.esther.bebergua_lembrete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import devandroid.esther.bebergua_lembrete.model.CalcularIngestaoDiaria
+import java.text.NumberFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edit_peso: EditText
+    private lateinit var edit_idade: EditText
+    private lateinit var btn_calcular: Button
+    private lateinit var txt_resultado_ml: TextView
+    private lateinit var ic_redefinir_dados: ImageView
+
+    private lateinit var calcularIngestaoDiaria: CalcularIngestaoDiaria
+    private var resultadoML = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportActionBar!!.hide()
+        //supportActionBar!!.hide()
+        iniciarComponentes()
+        calcularIngestaoDiaria= CalcularIngestaoDiaria()
+
+        btn_calcular.setOnClickListener{
+
+            if(edit_peso.text.toString().isEmpty()){
+                Toast.makeText(this, R.string.toast_informe_peso, Toast.LENGTH_SHORT).show()
+            }else if(edit_idade.text.toString().isEmpty()){
+                Toast.makeText(this, R.string.toast_informe_idade, Toast.LENGTH_SHORT).show()
+            }else{
+                val peso = edit_peso.text.toString().toDouble()
+                val idade = edit_idade.text.toString().toInt()
+                calcularIngestaoDiaria.calcularTotalML(peso, idade)
+                resultadoML = calcularIngestaoDiaria.resultadoML()
+                val formatar = NumberFormat.getNumberInstance(Locale("pt", "BR"))
+                formatar.isGroupingUsed = false
+                txt_resultado_ml.text = formatar.format(resultadoML)+ " " + "ml"
+            }
+        }
+
+        ic_redefinir_dados.setOnClickListener{
+            val alertDialog = AlertDialog.Builder(this)
+            alertDialog.setTitle(R.string.dialogo_titulo)
+                .setMessage(R.string.dialogo_desc)
+                .setPositiveButton("Ok", { dialogInterface, i ->
+                    edit_peso.setText("")
+                    edit_idade.setText("")
+                    txt_resultado_ml.text = ""
+                })
+            alertDialog.setNegativeButton( "Cancelar", { dialogoInterface, i ->
+
+            })
+            val dialog = alertDialog.create()
+            dialog.show()
+        }
+
+    }
+    private fun iniciarComponentes(){
+        edit_peso = findViewById(R.id.edit_peso)
+        edit_idade = findViewById(R.id.edit_idade)
+        btn_calcular = findViewById(R.id.btn_calcular)
+        txt_resultado_ml = findViewById(R.id.txt_resultado_ml)
+        ic_redefinir_dados = findViewById(R.id.ic_redefinir)
     }
 }
