c92823cf17c1b647ffd1834e60e71a687bdab04e
EstherAzevedo/ <estherazvdo123@gmail.com>
2024-01-13 10:54:42 -0300

    término do btn alarme e lembrete com findById

--- BeberguaLembrete/app/src/main/java/devandroid/esther/bebergua_lembrete/MainActivity.kt
+++ BeberguaLembrete/app/src/main/java/devandroid/esther/bebergua_lembrete/MainActivity.kt
@@ -1,15 +1,21 @@
 package devandroid.esther.bebergua_lembrete
 
+import android.annotation.SuppressLint
+import android.app.TimePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import devandroid.esther.bebergua_lembrete.model.CalcularIngestaoDiaria
 import java.text.NumberFormat
+import java.util.Calendar
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -19,9 +25,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btn_calcular: Button
     private lateinit var txt_resultado_ml: TextView
     private lateinit var ic_redefinir_dados: ImageView
+    private lateinit var btn_lembrate: Button
+    private lateinit var btn_alarme: Button
+    private lateinit var txt_hora: TextView
+    private lateinit var txt_minutos: TextView
 
     private lateinit var calcularIngestaoDiaria: CalcularIngestaoDiaria
     private var resultadoML = 0.0
+
+    lateinit var timePickerDialog: TimePickerDialog
+    lateinit var calendario: Calendar
+    var horaAtual = 0
+    var minutosAtuais = 0
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -63,6 +80,32 @@ class MainActivity : AppCompatActivity() {
             dialog.show()
         }
 
+        btn_lembrate.setOnClickListener{
+
+            calendario = Calendar.getInstance()
+            horaAtual = calendario.get(Calendar.HOUR_OF_DAY)
+            minutosAtuais = calendario.get(Calendar.MINUTE)
+            timePickerDialog = TimePickerDialog(this, { timePicker: TimePicker, hourOfDay: Int, minutes: Int ->
+                txt_hora.text= String.format("%02d", hourOfDay)
+                txt_minutos.text= String.format("%02d", minutes)
+            },horaAtual, minutosAtuais, true)
+            timePickerDialog.show()
+        }
+
+        btn_alarme.setOnClickListener {
+
+            if (!txt_hora.text.toString().isEmpty() && !txt_minutos.text.toString().isEmpty()) {
+                val intent = Intent(AlarmClock.ACTION_SET_ALARM)
+                intent.putExtra(AlarmClock.EXTRA_HOUR, txt_hora.text.toString().toInt())
+                intent.putExtra(AlarmClock.EXTRA_MINUTES, txt_minutos.text.toString().toInt())
+                intent.putExtra(AlarmClock.EXTRA_MESSAGE, getString(R.string.alarme_mensagem))
+                startActivity(intent)
+
+                if(intent.resolveActivity(packageManager) != null){
+                    startActivity(intent)
+                }
+            }
+        }
     }
     private fun iniciarComponentes(){
         edit_peso = findViewById(R.id.edit_peso)
@@ -70,5 +113,9 @@ class MainActivity : AppCompatActivity() {
         btn_calcular = findViewById(R.id.btn_calcular)
         txt_resultado_ml = findViewById(R.id.txt_resultado_ml)
         ic_redefinir_dados = findViewById(R.id.ic_redefinir)
+        btn_lembrate = findViewById(R.id.btn_definir_lembrete)
+        btn_alarme = findViewById(R.id.btn_alarme)
+        txt_hora = findViewById(R.id.txt_hora)
+        txt_minutos = findViewById(R.id.txt_minutos)
     }
 }
